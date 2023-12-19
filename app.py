82241e03b31807c3781f89635918a6d1768c7e63
Ankesh <ankesh3905222@gmail.com>
2023-12-19 16:06:51 +0530

    Solved same prediction output issue

--- app.py
+++ app.py
@@ -1,86 +1,74 @@
 from flask import Flask, jsonify, request
 import numpy as np
 import pandas as pd
-from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from datetime import datetime
 
 app = Flask(__name__)
+
+# Initialize an empty DataFrame for model training
+model_data = pd.DataFrame(columns=["amount", "time"])
 
 @app.route('/', methods=['POST'])
 def ExpensePredictor():
     if request.method == 'POST':
         # Access the data sent with the POST request
         data = request.json  # Assuming the data is in JSON format
-        # print(data["data"],data["category"])
 
-        # Process the data
-        result = process_data(data["data"], data["category"])
+        # Process the data for training
+        train_model(data["data"])
+
+        # Process the data for prediction
+        result = process_data(data["predict_data"])
         return jsonify({'result': result})
 
 
-def process_data(data, category):
-    print(data)  # Verify the structure of the input data
-
-    # Convert data to DataFrame
-    df = pd.DataFrame(data)
-
-    df["category"] = category
-        
-
+def train_model(train_data):
+    global model_data
+    # Convert training data to DataFrame
+    df = pd.DataFrame(train_data)
     df["time"] = pd.to_datetime(df["time"])
 
+    # Concatenate with existing model_data
+    model_data = pd.concat([model_data, df], ignore_index=True)
+
+    # Extract features (time) and target variable (amount)
+    model_data['day_of_week'] = model_data['time'].dt.dayofweek
+    X_train = model_data[["time", "day_of_week"]]
+    y_train = model_data["amount"]
+
     # Convert Timestamps to Unix timestamps (int64)
-    df["time_unix"] = (df["time"].astype(np.int64) // 10**9)
+    X_train["time"] = (X_train["time"].astype(np.int64) // 10**9)
 
-    # Encode the "Category" variable
-    label_encoder = LabelEncoder()
-    df["category_encoded"] = label_encoder.fit_transform(df["category"])
-
-    # Extract features (time and category) and target variable (amount)
-    X = df[["time_unix", "category_encoded"]]
-    y = df["amount"]
-
-    # Create a ColumnTransformer with a StandardScaler for the time feature
-    time_transformer = Pipeline([
-        ('scaler', StandardScaler())
+    # Create and train the linear regression model
+    global model
+    model = Pipeline([
+        ('scaler', StandardScaler()),
+        ('regressor', LinearRegression())
     ])
+    model.fit(X_train, y_train)
 
-    # Combine transformers using ColumnTransformer
-    preprocessor = ColumnTransformer(
-        transformers=[
-            ('time', time_transformer, ['time_unix']),
-        ]
-    )
 
-    # Create a linear regression model
-    model = LinearRegression()
+def process_data(predict_data):
+    # Convert prediction data to DataFrame
+    df = pd.DataFrame(predict_data)
+    df["time"] = pd.to_datetime(df["time"])
 
-    # Create a pipeline with preprocessing and the linear regression model
-    pipeline = Pipeline([
-        ('preprocessor', preprocessor),
-        ('model', model)
-    ])
+    # Extract features (time) for prediction
+    df['day_of_week'] = df['time'].dt.dayofweek
+    X_predict = df[["time", "day_of_week"]]
 
-    # Train the model using the entire dataset
-    pipeline.fit(X, y)
+    # Convert Timestamps to Unix timestamps (int64)
+    X_predict["time"] = (X_predict["time"].astype(np.int64) // 10**9)
 
-    # Input data for predicting future expenses (adjust as needed)
-    input_time = pd.to_datetime(df['time'].iloc[0])
-    input_category = df['category'].iloc[0]
+    # Predict using the trained model
+    predicted_amount = model.predict(X_predict)
 
-    # Convert input time to UNIX timestamp
-    input_time_unix = input_time.timestamp() // 10**9
+    print(predicted_amount)
 
-    # Transform input data using the preprocessor
-    input_data = pd.DataFrame({'time_unix': [input_time_unix]})
-    predicted_amount = pipeline.predict(input_data)
-
-    print(f"Predicted Expense Amount for {input_category} on {input_time} : {predicted_amount}")
-    return {'predicted_amount': predicted_amount[0]}
+    return {'predicted_amount': predicted_amount.tolist()}
 
 @app.route('/server_activator')
 def server_activator():
--- predictor.py
+++ predictor.py
@@ -9,57 +9,45 @@ from datetime import datetime
 
 # Sample data
 data = [
-    {"amount": 5000, "time": "2023-12-15T18:08:21.258+00:00", "category": "Groceries"},
+    {"amount": 20, "time": "2023-12-15T18:08:21.258+00:00", "category": "Groceries"},
     {"amount": 10, "time": "2023-12-15T18:07:14.119+00:00", "category": "Clothing"},
-    {"amount": 300, "time": "2023-12-14T09:52:23.883+00:00", "category": "Groceries"},
-    {"amount": 5000, "time": "2023-12-14T09:52:11.664+00:00", "category": "Clothing"},
-    {"amount": 100, "time": "2022-03-14T09:52:11.664+00:00", "category": "Groceries"},
-    {"amount": 4000, "time": "2023-08-14T09:52:11.664+00:00", "category": "Groceries"},
-    {"amount": 500, "time": "2023-02-14T09:52:11.664+00:00", "category": "Groceries"},
-    {"amount": 5000, "time": "2023-01-14T09:52:11.664+00:00", "category": "Groceries"},
+    {"amount": 30, "time": "2023-12-14T09:52:23.883+00:00", "category": "Groceries"},
+    {"amount": 300, "time": "2023-12-14T09:52:11.664+00:00", "category": "Clothing"},
+    {"amount": 1, "time": "2022-03-14T09:52:11.664+00:00", "category": "Groceries"},
+    {"amount": 40, "time": "2023-08-14T09:52:11.664+00:00", "category": "Groceries"},
+    {"amount": 40, "time": "2023-02-14T09:52:11.664+00:00", "category": "Groceries"},
+    {"amount": 50, "time": "2023-01-14T09:52:11.664+00:00", "category": "Groceries"},
 ]
 
 # Convert data to DataFrame
 df = pd.DataFrame(data)
 df["time"] = pd.to_datetime(df["time"])
 
-# Convert Timestamps to Unix timestamps (int64)
-df["time"] = (df["time"].astype(np.int64) // 10**9)
+# Extract features (time and category) and target variable (amount)
+df['day_of_week'] = df['time'].dt.dayofweek  # add day of week as a feature
+X = df[["time", "category", "day_of_week"]]
+y = df["amount"]
 
 # Encode the "Category" variable
 label_encoder = LabelEncoder()
-df["category_encoded"] = label_encoder.fit_transform(df["category"])
+X["category_encoded"] = label_encoder.fit_transform(X["category"])
 
-# Extract features (time and category) and target variable (amount)
-X = df[["time", "category_encoded"]]
-y = df["amount"]
+# Convert Timestamps to Unix timestamps (int64)
+X["time"] = (X["time"].astype(np.int64) // 10**9)
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Create a ColumnTransformer with a StandardScaler for the time feature
-preprocessor = ColumnTransformer(
-    transformers=[
-        ('time', 'passthrough', ['time']),
-        ('category', 'passthrough', ['category_encoded'])  # Include category in the model
-    ]
-)
+X_train, X_test, y_train, y_test = train_test_split(X[["time", "category_encoded", "day_of_week"]], y, test_size=0.2, random_state=42)
 
 # Create a linear regression model
 model = LinearRegression()
 
-# Create a pipeline with preprocessing and the linear regression model
-pipeline = Pipeline([
-    ('preprocessor', preprocessor),
-    ('model', model)
-])
-
 # Train the model
-pipeline.fit(X_train, y_train)
+model.fit(X_train, y_train)
 
 # Input data for predicting future expenses (adjust as needed)
-input_time = datetime.strptime("2024-03-22", "%Y-%m-%d")
-input_category = "Clothing"
+input_time = datetime.strptime("2024-12-20", "%Y-%m-%d")
+input_category = "Groceries"
+input_day_of_week = input_time.weekday()
 
 # Convert input time to UNIX timestamp
 input_time_unix = input_time.timestamp() // 10**9
@@ -67,8 +55,7 @@ input_time_unix = input_time.timestamp() // 10**9
 # Encode the input category
 input_category_encoded = label_encoder.transform([input_category])
 
-# Transform input data using the preprocessor
-input_data = pd.DataFrame({'time': [input_time_unix], 'category_encoded': input_category_encoded})
-predicted_amount = pipeline.predict(input_data)
+# Predict using the trained model
+predicted_amount = model.predict([[input_time_unix, input_category_encoded[0], input_day_of_week]])
 
 print(predicted_amount)
